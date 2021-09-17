c633a8c987e7b950d83bb6a448f984daa0b5af7e
carteak004 <carteak004@gmail.com>
2021-09-17 01:06:51 -0500

    used default styling to show dark and light modes

--- src/components/CommitComponent.js
+++ src/components/CommitComponent.js
@@ -2,19 +2,48 @@ import React from 'react';
 import {View, Text, useColorScheme} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
+import styles from '../styles/StyleSheet';
+
 const CommitComponent = ({history}) => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const {name, hash, message} = history;
 
-  const {author, hash, message} = history;
   return (
-    <View style={backgroundStyle}>
-      <Text style={{color: 'red'}}>{author}</Text>
-      <Text>{hash}</Text>
-      <Text>{message}</Text>
+    <View
+      style={[
+        styles.flatListView,
+        {
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        },
+      ]}>
+      <Text
+        style={[
+          styles.nameStyle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {name}
+      </Text>
+      <Text
+        style={[
+          styles.hashStyle,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {hash}
+      </Text>
+      <Text
+        style={[
+          styles.messageStyle,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {message}
+      </Text>
     </View>
   );
 };
