858e30dded3f37a9723574b6590252db24014e38
carteak004 <carteak004@gmail.com>
2021-09-17 01:09:26 -0500

    added different view for error, loading and style

--- src/containers/Commitcontainer.js
+++ src/containers/Commitcontainer.js
@@ -1,41 +1,88 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState} from 'react';
-import {FlatList, View, Text, SafeAreaView, useColorScheme} from 'react-native';
-
+import {
+  View,
+  FlatList,
+  Text,
+  ActivityIndicator,
+  useColorScheme,
+} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-import getCommits from '../api/GetCommits';
+import {getCommits} from '../api/GetCommits';
+import styles from '../styles/StyleSheet';
 import CommitComponent from '../components/CommitComponent';
 
-const CommitContainer = async () => {
+const CommitContainer = () => {
   const [commitHistory, setCommitHistory] = useState(null);
+  const [dataError, setDataError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
   useEffect(() => {
-    const getHistory = async () => {
-      const test = [{hash: '1', message: 'test', name: 'testing'}]; //await getCommits();
-      console.log('test', test);
-      setCommitHistory(test);
-      console.log('hist', commitHistory);
-    };
-    getHistory();
+    setIsLoading(true);
+    getCommits(
+      res => {
+        setCommitHistory(res);
+      },
+      () => {
+        setDataError(true);
+      },
+    );
+    setIsLoading(false);
   }, []);
-  return (
-    // <SafeAreaView>
-    <View style={backgroundStyle}>
-      <Text style={{color: 'red'}}>Hello world</Text>
-      <View style={{flex: 1}}>
+
+  const renderSeperator = () => {
+    return <View style={styles.seperatorStyle} />;
+  };
+  if (!isLoading) {
+    if (!dataError) {
+      return (
         <FlatList
+          ItemSeparatorComponent={renderSeperator}
           contentContainerStyle={{flexGrow: 1}}
           data={commitHistory}
-          renderItem={({history}) => <CommitComponent history={history} />}
+          renderItem={({item}) => <CommitComponent history={item} />}
         />
-      </View>
-      <Text style={{color: 'red'}}>Hello world</Text>
+      );
+    } else {
+      return (
+        <View
+          style={[
+            styles.errorView,
+            {
+              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            },
+          ]}>
+          <Text
+            style={[
+              // styles.errorStyle,
+              styles.messageStyle,
+              {
+                color: isDarkMode ? Colors.white : Colors.black,
+              },
+            ]}>
+            Error retreiving data!
+          </Text>
+        </View>
+      );
+    }
+  }
+  return (
+    <View
+      style={[
+        styles.flatListView,
+        {
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        },
+      ]}>
+      <ActivityIndicator
+        size="large"
+        color={isDarkMode ? Colors.white : Colors.black}
+        animating={true}
+      />
     </View>
-    // </SafeAreaView>
   );
 };
 
