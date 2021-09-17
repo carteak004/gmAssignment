38ff22be56295b272d79d47b177409cc43413ff4
carteak004 <carteak004@gmail.com>
2021-09-17 01:10:48 -0500

    changed the url to show react native's commits

--- src/api/Constants.js
+++ src/api/Constants.js
@@ -1,3 +1,2 @@
 export const COMMITSURL =
-  'https://api.github.com/repos/carteak004/gmAssignment/commits';
-//   https://api.github.com/repos/facebook/react-native/commits
+  'https://api.github.com/repos/facebook/react-native/commits';
